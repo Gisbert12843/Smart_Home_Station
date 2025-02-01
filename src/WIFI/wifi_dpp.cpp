fa48fc1f3cbb62b03d9889014656e39b2e4dcb40
Kai Oestreich <kai.oestreich@alumni.fh-aachen.de>
2025-02-01 14:31:44 +0100

    fixed bug causing memory allocation failure when running DPP routine a second time

--- src/WIFI/wifi_dpp.cpp
+++ src/WIFI/wifi_dpp.cpp
@@ -6,12 +6,12 @@ void dpp_enrollee_event_cb(esp_supp_dpp_event_t event, void *data)
 {
     constexpr const char *TAG = "dpp_enrollee_event_cb()";
     esp_err_t err;
-    ESP_LOGI(TAG, "dpp_enrollee_event_cb called with event: %d", event);
+    //ESP_LOGI(TAG, "dpp_enrollee_event_cb called with event: %d", event);
 
     switch (event)
     {
     case ESP_SUPP_DPP_URI_READY:
-        ESP_LOGI(TAG, "ESP_SUPP_DPP_URI_READY event");
+        //ESP_LOGI(TAG, "ESP_SUPP_DPP_URI_READY event");
         if (data != NULL)
         {
 
@@ -23,24 +23,24 @@ void dpp_enrollee_event_cb(esp_supp_dpp_event_t event, void *data)
                 ESP_LOGE(TAG, "Failed to start DPP listen: %s", esp_err_to_name(err));
                 xEventGroupSetBits(WiFi_Functions::wifi_event_group, WiFi_Functions::DPP_AUTH_FAIL_BIT);
             }
-            ESP_LOGI(TAG, "Started esp_supp_dpp_start_listen");
+            //ESP_LOGI(TAG, "Started esp_supp_dpp_start_listen");
         }
         break;
     case ESP_SUPP_DPP_CFG_RECVD:
     {
-        ESP_LOGI(TAG, "ESP_SUPP_DPP_CFG_RECVD event");
+        //ESP_LOGI(TAG, "ESP_SUPP_DPP_CFG_RECVD event");
         s_retry_num = 0;
 
         memcpy(&s_dpp_wifi_config, data, sizeof(s_dpp_wifi_config));
         if (s_dpp_wifi_config.sta.ssid[0] == '\0' || s_dpp_wifi_config.sta.password[0] == '\0')
         {
-            ESP_LOGI(TAG, "SSID or password is null, setting DPP_AUTH_FAIL_BIT");
+            //ESP_LOGI(TAG, "SSID or password is null, setting DPP_AUTH_FAIL_BIT");
             xEventGroupSetBits(WiFi_Functions::wifi_event_group, WiFi_Functions::DPP_AUTH_FAIL_BIT);
             break;
         }
         size_t ssid_len = strlen((char *)s_dpp_wifi_config.sta.ssid);
         size_t password_len = strlen((char *)s_dpp_wifi_config.sta.password);
-        ESP_LOGI(TAG, "Received Wi-Fi configuration: SSID: %s, Password: %s", (char *)s_dpp_wifi_config.sta.ssid, (char *)s_dpp_wifi_config.sta.password);
+        //ESP_LOGI(TAG, "Received Wi-Fi configuration: SSID: %s, Password: %s", (char *)s_dpp_wifi_config.sta.ssid, (char *)s_dpp_wifi_config.sta.password);
 
         err = esp_wifi_set_config(WIFI_IF_STA, &s_dpp_wifi_config);
 
@@ -53,33 +53,33 @@ void dpp_enrollee_event_cb(esp_supp_dpp_event_t event, void *data)
 
         if (ssid_len == 0 || password_len == 0 || ssid_len > sizeof(s_dpp_wifi_config.sta.ssid) - 1 || password_len > sizeof(s_dpp_wifi_config.sta.password) - 1)
         {
-            ESP_LOGI(TAG, "SSID or password is empty or too long, setting DPP_AUTH_FAIL_BIT");
+            //ESP_LOGI(TAG, "SSID or password is empty or too long, setting DPP_AUTH_FAIL_BIT");
             xEventGroupSetBits(WiFi_Functions::wifi_event_group, WiFi_Functions::DPP_AUTH_FAIL_BIT);
             break;
         }
 
-        ESP_LOGI(TAG, "Saving AP Configuration: SSID: %s, Password: %s", (char *)s_dpp_wifi_config.sta.ssid, (char *)s_dpp_wifi_config.sta.password);
+        //ESP_LOGI(TAG, "Saving AP Configuration: SSID: %s, Password: %s", (char *)s_dpp_wifi_config.sta.ssid, (char *)s_dpp_wifi_config.sta.password);
 
         WiFi_Functions::AP_Credentials::saveAPConfiguration((char *)s_dpp_wifi_config.sta.ssid, (char *)s_dpp_wifi_config.sta.password);
 
         if (WiFi_Functions::connect_to_ap(WiFi_Functions::AP_Credentials::getInstance()))
         {
-            ESP_LOGI(TAG, "Successfully connected to AP");
+            //ESP_LOGI(TAG, "Successfully connected to AP");
             xEventGroupSetBits(WiFi_Functions::wifi_event_group, WiFi_Functions::WIFI_CONNECTED_BIT);
         }
         else
         {
-            ESP_LOGI(TAG, "Failed to connect to AP, setting DPP_AUTH_FAIL_BIT");
+            //ESP_LOGI(TAG, "Failed to connect to AP, setting DPP_AUTH_FAIL_BIT");
             xEventGroupSetBits(WiFi_Functions::wifi_event_group, WiFi_Functions::DPP_AUTH_FAIL_BIT);
         }
     }
     break;
     case ESP_SUPP_DPP_FAIL:
     {
-        ESP_LOGI(TAG, "ESP_SUPP_DPP_FAIL event");
+        //ESP_LOGI(TAG, "ESP_SUPP_DPP_FAIL event");
         if (s_retry_num < 10)
         {
-            ESP_LOGI(TAG, "DPP Auth failed (Reason: %s), retrying...", esp_err_to_name((int)data));
+            //ESP_LOGI(TAG, "DPP Auth failed (Reason: %s), retrying...", esp_err_to_name((int)data));
             err = (esp_supp_dpp_start_listen());
             if (err != ESP_OK)
             {
@@ -90,14 +90,14 @@ void dpp_enrollee_event_cb(esp_supp_dpp_event_t event, void *data)
         }
         else
         {
-            ESP_LOGI(TAG, "Retry limit reached, setting DPP_AUTH_FAIL_BIT");
+            //ESP_LOGI(TAG, "Retry limit reached, setting DPP_AUTH_FAIL_BIT");
             xEventGroupSetBits(WiFi_Functions::wifi_event_group, WiFi_Functions::DPP_AUTH_FAIL_BIT);
         }
     }
     break;
     default:
     {
-        ESP_LOGI(TAG, "Unhandled DPP event: %d", event);
+        //ESP_LOGI(TAG, "Unhandled DPP event: %d", event);
     }
     break;
     }
@@ -106,7 +106,7 @@ void dpp_enrollee_event_cb(esp_supp_dpp_event_t event, void *data)
 esp_err_t dpp_enrollee_bootstrap(void)
 {
     constexpr const char *TAG = "dpp_enrollee_bootstrap()";
-    ESP_LOGI(TAG, "dpp_enrollee_bootstrap called");
+    //ESP_LOGI(TAG, "dpp_enrollee_bootstrap called");
 
     esp_err_t ret;
     size_t pkey_len = strlen(EXAMPLE_DPP_BOOTSTRAPPING_KEY);
@@ -114,20 +114,20 @@ esp_err_t dpp_enrollee_bootstrap(void)
 
     if (pkey_len)
     {
-        ESP_LOGI(TAG, "Generating bootstrapping key");
+        //ESP_LOGI(TAG, "Generating bootstrapping key");
         char prefix[] = "30310201010420";
         char postfix[] = "a00a06082a8648ce3d030107";
 
         if (pkey_len != CURVE_SEC256R1_PKEY_HEX_DIGITS)
         {
-            ESP_LOGI(TAG, "Invalid key length! Private key needs to be 32 bytes (or 64 hex digits) long");
+            //ESP_LOGI(TAG, "Invalid key length! Private key needs to be 32 bytes (or 64 hex digits) long");
             return ESP_FAIL;
         }
 
         key = (char *)malloc(sizeof(prefix) + pkey_len + sizeof(postfix));
         if (!key)
         {
-            ESP_LOGI(TAG, "Failed to allocate memory for bootstrapping key");
+            //ESP_LOGI(TAG, "Failed to allocate memory for bootstrapping key");
             return ESP_ERR_NO_MEM;
         }
         sprintf(key, "%s%s%s", prefix, EXAMPLE_DPP_BOOTSTRAPPING_KEY, postfix);
@@ -144,9 +144,10 @@ esp_err_t dpp_enrollee_bootstrap(void)
 bool dpp_enrollee_init(void)
 {
     constexpr const char *TAG = "dpp_enrollee_init()";
-    ESP_LOGI(TAG, "dpp_enrollee_init called");
+    //ESP_LOGI(TAG, "dpp_enrollee_init called");
 
     esp_err_t err;
+    WiFi_Functions::de_init();
 
     WiFi_Functions::init();
 
@@ -158,7 +159,7 @@ bool dpp_enrollee_init(void)
 
     vTaskDelay(pdMS_TO_TICKS(600));
 
-    ESP_LOGI(TAG, "Initializing DPP");
+    //ESP_LOGI(TAG, "Initializing DPP");
     err = esp_supp_dpp_init(dpp_enrollee_event_cb);
 
     std::shared_ptr<DPP_QR_Code> dpp_qr_code;
@@ -171,7 +172,7 @@ bool dpp_enrollee_init(void)
         return false;
     }
 
-    ESP_LOGI(TAG, "Bootstrapping DPP");
+    //ESP_LOGI(TAG, "Bootstrapping DPP");
     err = (dpp_enrollee_bootstrap());
     if (err != ESP_OK)
     {
@@ -182,7 +183,7 @@ bool dpp_enrollee_init(void)
         return false;
     }
 
-    ESP_LOGI(TAG, "Starting Wi-Fi");
+    //ESP_LOGI(TAG, "Starting Wi-Fi");
     err = esp_wifi_start();
     if (err != ESP_OK)
     {
@@ -192,7 +193,7 @@ bool dpp_enrollee_init(void)
         return false;
     }
 
-    ESP_LOGI(TAG, "Waiting for DPP_BITS");
+    //ESP_LOGI(TAG, "Waiting for DPP_BITS");
     EventBits_t bits = xEventGroupWaitBits(WiFi_Functions::wifi_event_group,
                                            WiFi_Functions::WIFI_CONNECTED_BIT | WiFi_Functions::DPP_AUTH_FAIL_BIT | WiFi_Functions::DPP_TIMEOUT_BIT | WiFi_Functions::DPP_ERROR_BIT,
                                            pdFALSE,
@@ -200,13 +201,13 @@ bool dpp_enrollee_init(void)
                                            pdMS_TO_TICKS(200000)); // Adding a timeout to prevent indefinite waiting
     std::lock_guard<std::recursive_mutex> lock(dpp_qr_code.get()->get_mutex());
 
-    ESP_LOGI(TAG, "Deinitializing DPP");
+    //ESP_LOGI(TAG, "Deinitializing DPP");
     esp_supp_dpp_deinit();
 
     if (bits & WiFi_Functions::WIFI_CONNECTED_BIT)
     {
-        ESP_LOGI(TAG, "Connected to AP");
-        ESP_LOGI(TAG, "dpp_enrollee_init finished");
+        //ESP_LOGI(TAG, "Connected to AP");
+        //ESP_LOGI(TAG, "dpp_enrollee_init finished");
         qr_code_ref = nullptr;
 
         return true;
@@ -250,8 +251,8 @@ bool dpp_enrollee_init(void)
 void dpp_task_start(void *params)
 {
     constexpr const char *TAG = "dpp_task_start()";
-    ESP_LOGI(TAG, "dpp_task_start START");
+    //ESP_LOGI(TAG, "dpp_task_start START");
     dpp_enrollee_init();
-    ESP_LOGI(TAG, "dpp_task_start END");
+    //ESP_LOGI(TAG, "dpp_task_start END");
     vTaskDelete(NULL);
 }
